e2e40155df8bbc9c2a8cfd14bb53a693bb83d4a2
phamdung3009 <hoangdung1999.vn@gmail.com>
2024-11-18 14:23:30 +0700

    commit demo spring

---
+++ src/test/java/demo/spring/boot/demospringboot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.spring.boot.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
